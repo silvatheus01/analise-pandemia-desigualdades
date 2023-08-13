62dcff8014f202c71b7396a47fc758235564f69d
Matheus Silva <matheus.car.ms@gmail.com>
2023-08-13 17:26:51 -0300

    Backup

--- src.py
+++ src.py
@@ -3,11 +3,12 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+PROJECT_ID = "base-dos-dados-395523"
 
 # Para carregar o dado direto no pandas
 # df = bd.read_table(dataset_id='br_inep_ideb',
 # table_id='brasil',
-# billing_project_id="base-dos-dados-395523")
+# billing_project_id=PROJECT_ID)
 
 # df.to_excel('br_inep_ideb.xlsx', index=False)
 
@@ -73,45 +74,129 @@ import matplotlib.pyplot as plt
 #     print(f'Media no ano {ano}: {media}')
 
 ##############
-#Total de furtos
+#Taxa de abandono
+# df = bd.read_table(dataset_id='br_inep_censo_escolar',
+# table_id='escola',
+# billing_project_id=PROJECT_ID)
 
-df = pd.read_csv('DOMensalEstadoDesde1991.csv', sep=";")
-# filtro = (((df['ano'] == 2020) & (df['mes'] >= 2)) | ((df['ano'] > 2020) & (df['ano'] <= 2021)))
-# df_filtrado = df[filtro]
+# df.to_excel('br_inep_censo_escolar__escola.xlsx', index=False)
 
-# df_filtrado['data'] = df_filtrado['mes'].astype(str) + '-' + df_filtrado['ano'].astype(str)
-# df_filtrado['data'] = pd.to_datetime(df_filtrado['data'], format='%m-%Y')
-# df_filtrado = df_filtrado.sort_values(by='data')
+# df = bd.read_table(
+#     dataset_id='br_inep_indicadores_educacionais',
+#     table_id='brasil',
+#     billing_project_id=PROJECT_ID
+# )
 
-# plt.figure(figsize=(10, 6))
-# plt.plot(df_filtrado['data'], df_filtrado['total_furtos'], marker='o')
-# plt.xlabel('Data (Mês-Ano)')
-# plt.ylabel('Total de Furtos')
-# plt.title('Furtos entre fevereiro de 2020 e dezembro de 2021')
-# plt.xticks(rotation=45)
-# plt.grid(True)
-# plt.tight_layout()
+# df.to_excel('br_inep_indicadores_educacionais__brasil.xlsx', index=False)
 
-# plt.savefig('grafico_furtos.png')
+# Tratamento
+# df = pd.read_excel('br_inep_indicadores_educacionais__brasil.xlsx')
+# df = df[(df['localizacao'] == 'total') & ((df['rede'] == 'publica') | (df['rede'] == 'privada'))]
+# df.sort_values('ano', inplace=True)
+# df.to_csv('br_inep_indicadores_educacionais__brasil__tratado.csv')
 
-filtro = (df['ano'] >= 2017) & (df['ano'] <= 2022)
-df = df[filtro]
 
-tipos_roubo = [
-    'roubo_transeunte', 'roubo_celular', 'roubo_em_coletivo', 'roubo_rua',
-    'roubo_veiculo', 'roubo_carga', 'roubo_comercio', 'roubo_residencia',
-    'roubo_banco', 'roubo_cx_eletronico', 'roubo_conducao_saque',
-    'roubo_apos_saque', 'roubo_bicicleta', 'outros_roubos', 'total_roubos'
-]
+# taxas_abandono = {
+#     'taxa_abandono_ef':'Ensino Fundamental', 
+#     'taxa_abandono_ef_anos_iniciais':'Ensino Fundamental Anos Iniciais', 
+#     'taxa_abandono_ef_anos_finais':'Ensino Fundamental Anos Finais',
+#     'taxa_abandono_ef_1_ano':'Ensino Fundamental - 1º ano', 
+#     'taxa_abandono_ef_2_ano':'Ensino Fundamental - 2º ano', 
+#     'taxa_abandono_ef_3_ano':'Ensino Fundamental - 3º ano',
+#     'taxa_abandono_ef_4_ano':'Ensino Fundamental - 4º ano', 
+#     'taxa_abandono_ef_5_ano':'Ensino Fundamental - 5º ano' ,
+#     'taxa_abandono_ef_6_ano':'Ensino Fundamental - 6º ano',
+#     'taxa_abandono_ef_7_ano':'Ensino Fundamental - 7º ano', 
+#     'taxa_abandono_ef_8_ano':'Ensino Fundamental - 8º ano', 
+#     'taxa_abandono_ef_9_ano':'Ensino Fundamental - 9º ano',
+#     'taxa_abandono_em':'Ensino Médio', 
+#     'taxa_abandono_em_1_ano':'Ensino Médio - 1º ano',
+#     'taxa_abandono_em_2_ano':'Ensino Médio - 2º ano',
+#     'taxa_abandono_em_3_ano':'Ensino Médio - 3º ano', 
+#     'taxa_abandono_em_4_ano':'Ensino Médio - 4º ano' , 
+#     'taxa_abandono_em_nao_seriado':'Ensino Médio Não seriado'
+# }
 
-medias = {}
+# anos = [2018,2019,2020,2021]
+# posicao = np.arange(len(anos))
+# barWidth = 0.25
+# df = pd.read_csv('br_inep_indicadores_educacionais__brasil__tratado.csv')
 
-for ano in df['ano'].unique():
-    for tipo in tipos_roubo:
-        df_filtrado = df[df['ano'] == ano]
-        media = df_filtrado[tipo].mean()
-        medias[(tipo, ano)] = media
+# def legend_without_duplicate_labels(figure):
+#     handles, labels = plt.gca().get_legend_handles_labels()
+#     by_label = dict(zip(labels, handles))
+#     figure.legend(by_label.values(), by_label.keys())
 
-for key, value in medias.items():
-    if(key[0] == 'roubo_rua'):
-        print(key, value)
+# for taxa, label in taxas_abandono.items():
+#     posicoes = []
+#     for i, ano in enumerate(anos):
+#         posicao_privada = i
+#         posicao_publica = i + barWidth
+#         posicoes.append((posicao_privada+posicao_publica)/2)
+        
+#         filtro_publica = (df['ano'] == ano) & (df['rede'] == 'publica')
+#         filtro_privada = (df['ano'] == ano) & (df['rede'] == 'privada')
+
+#         df_filtrado_publica = df[filtro_publica]
+#         df_filtrado_privada = df[filtro_privada]
+
+#         dados_abandono_publica = df_filtrado_publica[taxa]
+#         dados_abandono_privada = df_filtrado_privada[taxa]            
+
+#         plt.bar(posicao_privada, dados_abandono_privada, color= '#1f6eed', width=barWidth, label='privada')
+#         plt.bar(posicao_publica, dados_abandono_publica, color= '#59ed1f', width=barWidth, label='pública')
+
+#         plt.xlabel(f'Abandono: {label}')
+
+    
+#     plt.xticks(posicoes, anos)
+#     plt.ylabel('Taxa de abandono')
+#     plt.title('Taxa de abandono por ano na rede privada e pública')
+
+#     legend_without_duplicate_labels(plt)
+#     #plt.legend()
+#     plt.savefig(f'grafico_abandono_{taxa}')
+#     plt.clf()  
+
+## Gráfico de dispersão taxa de reprovação x esforço docente
+# df = bd.read_table(dataset_id='br_inep_indicadores_educacionais',
+# table_id='uf',
+# billing_project_id=PROJECT_ID)
+
+# df.to_csv('br_inep_indicadores_educacionais__uf.csv', index=False)
+
+df = pd.read_csv('br_inep_indicadores_educacionais__uf.csv')
+
+filtro = (df['localizacao'] == 'total') & (df['rede'] == 'total')
+df_filtrado = df[filtro]
+
+
+# df_filtrado_taxa_reprovacao = df_filtrado.filter(like='taxa_reprovacao_')
+# df_filtrado_had = df_filtrado.filter(like='had_')
+# df_filtrado_outros = df_filtrado.filter(items=['ano','sigla_uf','localizacao','rede']).sort_values('ano')
+
+#df_filtrado = pd.concat([df_filtrado_outros, df_filtrado_taxa_reprovacao, df_filtrado_had], axis=1)
+#df_filtrado.to_csv('br_inep_indicadores_educacionais__uf__tratado.csv')
+
+df_filtrado_outros = df_filtrado.filter(items=['ano','sigla_uf','localizacao','rede']).sort_values('ano')
+df_filtrado = pd.concat(
+    [
+        df_filtrado_outros,
+        df_filtrado['taxa_reprovacao_em'], 
+        df_filtrado['taxa_reprovacao_ef'], 
+        df_filtrado['had_ef'],
+        df_filtrado['had_em']
+    ],
+    axis=1
+)
+
+df_filtrado.to_csv('br_inep_indicadores_educacionais__uf__tratado.csv', index=False)
+ 
+# anos = [2018,2019,2020,2021,2022]
+# df = pd.read_csv('br_inep_indicadores_educacionais__uf__tratado.csv')
+# filtro = (df['ano'] == 2021) & (df['sigla_uf'] == 'BA')
+# df = df[filtro]
+
+# df_filtrado_taxa_reprovacao = df.filter(like='taxa_reprovacao_')
+
+# print(df[np.isnan(df['taxa_reprovacao_ef']) == False])
